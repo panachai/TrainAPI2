43ce9e8e26767d07756717dbc7b80cc609929af0
Panachai <panachai.ny@gmail.com>
2017-10-26 16:47:38 +0700

    Add project files.

---
+++ TrainAPI2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TrainAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
